b636f2e5ca8042a34eaa56443c75e25f88ae67c1
Antonio Bel <antonio.bel.puchol@gmail.com>
2020-05-27 16:35:35 +0200

    Version inicial en TS

--- guinoteserver.ts
+++ guinoteserver.ts
@@ -1,44 +1,50 @@
 /*
  * Copyright (C) 2020 Antonio Bel Puchol
  */
-/* global __dirname, path */
 
-const path = require('path');
-const express = require('express');
-const serveIndex = require('serve-index');
-const cors = require('cors');
-const http = require('http');
-const colyseus = require('colyseus');
-const monitor = require('@colyseus/monitor').monitor;
+import path from 'path';
+import express from 'express';
+import serveIndex from 'serve-index';
+import cors from 'cors';
+import { createServer } from 'http';
+import { Server } from 'colyseus';
+import { monitor } from '@colyseus/monitor';
 
-const Handler = require("./server/handler").Handler;
+import { Handler } from './server/handler';
 
-const port = 3000;
+const port =3000;
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-const server = http.createServer(app);
-const gameServer = new colyseus.Server({
-    server: server,
-    express: app,
-    pingInterval: 0
+// Attach WebSocket Server on HTTP Server.
+const gameServer = new Server({
+  server: createServer(app),
+  express: app,
+  pingInterval: 0,
 });
 
 gameServer.define("go_room", Handler);
 
 app.use('/', express.static(path.join(__dirname, "static")));
 app.use('/', serveIndex(path.join(__dirname, "static"), {'icons': true}));
-app.use('/colyseus', monitor(gameServer));
 app.use('/colyseusclient', express.static(path.join(__dirname, "/colyseusclient")));
 app.use('/server', express.static(path.join(__dirname, "/server")));
 app.use('/client', express.static(path.join(__dirname, "/client")));
 
+// (optional) attach web monitoring panel
+app.use('/colyseus', monitor());
+
 gameServer.onShutdown(function(){
-    console.log(`Guiñote Server finalizando.`);
+  console.log(`Guiñote Server finalizando.`);
 });
 
 gameServer.listen(port);
 
-console.log(`Guiñote Server funcionando en http://localhost:${ port }`);
+process.on("uncaughtException", (e) => {
+  console.log(e.stack);
+  process.exit(1);
+});
+
+console.log(`Listening on http://localhost:${ port }`);
